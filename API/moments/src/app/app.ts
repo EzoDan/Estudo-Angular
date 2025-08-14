78ca3e622ece09871fb8216685ccec908cef68df
Enzo Dan <enzodanner@gmail.com>
2025-08-14 12:22:46 -0300

    Avanços no projeto

--- API/moments/src/app/app.ts
+++ API/moments/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Footer } from "./components/footer/footer";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
