02748baa1a97211e94dfc9b5c3d7a2252896ce17
Enzo Dan <enzodanner@gmail.com>
2025-08-14 14:05:24 -0300

    Css inicial

--- API/moments/src/app/app.routes.ts
+++ API/moments/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/pages/home/home';
 import { About } from './components/pages/about/about';
+import { NewMoment } from './components/pages/new-moment/new-moment';
 
 export const routes: Routes = [
     {path: '', component: Home },
     {path: 'about', component: About },
+    {path: 'moments/new', component: NewMoment },
 ];
